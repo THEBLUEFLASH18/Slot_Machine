a3fdbb81a4e73c669a0890b82db7181c1135938e
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-08-03 11:50:06 -0400

    Added additional code for the deposit function

--- project.js
+++ project.js
@@ -2,7 +2,22 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    const depositAmount = prompt("Please deposit an amount: ")
+    while(true){
+        /* When this is true the infinite loop will continue until the person corrects the value they entered,
+        if the value they entered is good then the else{} will just return the parseFloat() amount to te 
+        function deposit  */
+            const depositAmount = prompt("Please deposit an amount: ")
+        /* parseFloat() can only return string(number) values into numbers, 
+        if depositAmount gave us back a string it would produce a NAN(not a number) value*/
+        const depositedAmount = parseFloat(depositAmount)
+        if(isNaN(depositedAmount) || depositedAmount <= 0){
+            alert("There is an improper value being used for the depost please check and correct!")
+        }
+        else{
+            return depositedAmount
+        }
+
+    }
 };
 
 deposit();
