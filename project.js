43689c02484e6974add49b5c77767266ef34f425
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-08-14 09:21:25 -0400

    added the rows separated by |

--- project.js
+++ project.js
@@ -107,6 +107,19 @@ const transpose = (reels) =>{
     }
 
     return rows 
+};
+
+const printRows = (rows) =>{
+    for(const row of rows){
+        let rowString = "";
+        for(const [i,symbol] of row.entries()){
+            rowString += symbol
+            if(i != row.length -1 ){
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
+    }
 }
 
 
@@ -115,6 +128,7 @@ const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
+printRows(rows)
 
 
 
