11c0437f25b577e3b92c2d4811f047294e18cd9b
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-08-14 09:09:48 -0400

    adding the transposed rows

--- project.js
+++ project.js
@@ -81,8 +81,9 @@ const spin = ()=>{
         }
     }
 
-    const reels = [[],[],[]]
+    const reels = []
     for(let i = 0; i < COLS; i++){
+        reels.push([])
         const reelSymbols = [...symbols]
         for(let j = 0; j < ROWS; j++){
             const randomIndex = Math.floor(Math.random() * reelSymbols.length)
@@ -96,10 +97,22 @@ const spin = ()=>{
 };
 
 
-const reels = spin();
+const transpose = (reels) =>{
+    const rows = [];
+    for(let i = 0; i < ROWS; i++){
+        rows.push([])
+        for(let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows 
+}
+
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
 
 
